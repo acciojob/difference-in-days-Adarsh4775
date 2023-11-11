f0a4826137d1521e918e50953c9fefe46c88a2b3
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-11-11 21:01:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	let difference = 0;
+	for(i=date1; i<date2; i++)
+		{
+			difference = date2 - date1;
+		}
+	     return difference;
+	if( date1 === date2)
+	{
+		return 0;
+	}
 };
 
 // Do not change the code below.
-const dateOne = prompt("Enter Start Date.");
-const dateTwo = prompt("Enter End Date.");
-alert(dateDiffInDays(dateOne, dateTwo));
+const date1 = parseFloat(prompt("Enter Start Date."));
+const date2 = parseFloat(prompt("Enter End Date."));
+alert(dateDiffInDays(date1, date2));  
